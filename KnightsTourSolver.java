8eb24003c1786fcefe8f6a47bc5690ab02c4fc95
trent-8 <trenthek@gmail.com>
2024-09-27 14:00:02 -0500

    make recursive

--- KnightsTourSolver.java
+++ KnightsTourSolver.java
@@ -15,57 +15,39 @@ public final class KnightsTourSolver {
      */
     static public boolean solveKnightsTour(KnightsTourBoard board, int x, int y, int num) {
         int numOfSquares = board.getBoardSize() * board.getBoardSize();
-        // stores a list of move lists so we can redo our first moves and
-        // keep track of the moves we still need to try
-        ArrayList<ArrayList<Integer[]>> testMoveLists = new ArrayList<>();
-        testMoveLists.add(getWarnsdorfRuleMoves(board, x, y));
-        ArrayList<Integer[]> validMoves = new ArrayList<>();
+        ArrayList<Integer[]> initialValidMoves = getWarnsdorfRuleMoves(board, x, y);
+        ArrayList<Integer[]> validMoves = new ArrayList<>(initialValidMoves);
         int nextSquare = num;
         int currentx = x;
         int currenty = y;
-        while (nextSquare <= numOfSquares) {
+        while (!validMoves.isEmpty()) {
+            currentx += validMoves.getFirst()[0];
+            currenty += validMoves.getFirst()[1];
+            board.setPositionValue(currentx, currenty, nextSquare);
+            nextSquare++;
             validMoves = getWarnsdorfRuleMoves(board, currentx, currenty);
-            // backtrack when a dead end is reached
-            if (validMoves.isEmpty()) {
-                // clear the board
-                for (int i = 0; i < board.getBoardSize(); i++) {
-                    for (int j = 0; j < board.getBoardSize(); j++)
-                        board.setPositionValue(i, j, -1);
-                }
-                // remove the move that didn't work if there are more than 1 left to test
-                if (testMoveLists.getLast().size() > 1) {
-                    testMoveLists.getLast().removeFirst();
-                }
-                // redo the starting moves
-                nextSquare = num;
-                currentx = x;
-                currenty = y;
-                board.setPositionValue(currentx, currenty, nextSquare - 1);
-                for (ArrayList<Integer[]> testMoveList : testMoveLists) {
-                    currentx += testMoveList.getFirst()[0];
-                    currenty += testMoveList.getFirst()[1];
-                    board.setPositionValue(currentx, currenty, nextSquare);
-                    nextSquare++;
-                }
-                // add another move if there are no more moves to try at the current backtracked space
-                if (testMoveLists.getLast().size() == 1) {
-                    testMoveLists.add(getWarnsdorfRuleMoves(board, currentx, currenty));
-                    currentx += testMoveLists.getLast().getFirst()[0];
-                    currenty += testMoveLists.getLast().getFirst()[1];
-                    board.setPositionValue(currentx, currenty, nextSquare);
-                    nextSquare++;
-                }
-            } else {
-                // make moves untill we hit a dead end or finish
-                currentx += validMoves.getFirst()[0];
-                currenty += validMoves.getFirst()[1];
-                board.setPositionValue(currentx, currenty, nextSquare);
-                nextSquare++;
+        }
+        if (nextSquare > numOfSquares) {
+            return true;
+        }
+        // backtrack the board
+        for (int i = 0; i < board.getBoardSize(); i++) {
+            for (int j = 0; j < board.getBoardSize(); j++) {
+                if (board.getPositionValue(i, j) >= num)
+                    board.setPositionValue(i, j, -1);
             }
         }
-        return true;
+        // try all other moves from this position
+        for (Integer[] move : initialValidMoves) {
+            board.setPositionValue(x+move[0], y+move[1], num);
+            if (solveKnightsTour(board, x+move[0], y+move[1], num + 1)) {
+                return true;
+            }
+            board.setPositionValue(x+move[0], y+move[1], -1);
+        }
+        return false;
     }
-    
+
     /**
      *
      * @param board
